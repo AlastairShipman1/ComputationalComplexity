sorted small bug with rotating before vehicle.dt was instantiated. about to change rotation model

model/Agent/EgoVehicle.py
@@ -71,30 +71,43 @@ class EgoVehicle(Vehicle):
         self.actor_point = None
         self.next_wp = self.find_first_waypoint()
 
+
+
     # region drawing functions
 
     def draw(self, surface):
         if self.perceiving:
             self.draw_sensor_rays(surface)
             self.draw_sensor_area(surface)
-
         self.draw_predicted_motion(surface)
         self.draw_closest_unknown_points(surface)
         self.draw_collision_points(surface)
         self.draw_collision_meta_data(surface)
         self.draw_waypoints(surface)
-
         super().draw(surface)
 
     def draw_waypoints(self, surface):
         for p in self.world.waypoints:
-            draw_forward_coords = self.convert_world_to_draw_coords(p.position)
-            if draw_forward_coords[0]>0:
-                pygame.draw.circle(surface, Colours.RED, draw_forward_coords, 10)
+            draw_coords = self.convert_world_to_draw_coords(p.position)
+            if draw_coords[0]>0 and draw_coords[1]>0:
+                pygame.draw.circle(surface, Colours.RED, draw_coords, 5)
+
+                arrow_width_angle= np.deg2rad(10)
+                arrow_width_length=15
+                arrow_length=25
+                heading = p.direction
+                end_point = [draw_coords[0]+arrow_length*np.cos(heading), draw_coords[1]+arrow_length*np.sin(heading)]
+                arrow_head_point_1 = [end_point[0] - arrow_width_length*np.cos(heading-arrow_width_angle), end_point[1]-arrow_width_length*np.sin(heading-arrow_width_angle)]
+                arrow_head_point_2 = [end_point[0] - arrow_width_length*np.cos(heading+arrow_width_angle), end_point[1]-arrow_width_length*np.sin(heading+arrow_width_angle)]
+
+                pygame.draw.aaline(surface, Colours.RED, draw_coords, end_point)
+                pygame.draw.aaline(surface, Colours.RED, end_point, arrow_head_point_1)
+                pygame.draw.aaline(surface, Colours.RED, end_point, arrow_head_point_2)
+
         if self.next_wp is not None:
-            draw_forward_coords = self.convert_world_to_draw_coords(self.next_wp.position)
-            if draw_forward_coords[0]>0:
-                pygame.draw.circle(surface, Colours.GREEN, draw_forward_coords, 10)
+            draw_coords = self.convert_world_to_draw_coords(self.next_wp.position)
+            if draw_coords[0]>0 and draw_coords[1]>0:
+                pygame.draw.circle(surface, Colours.GREEN, draw_coords, 5)
 
     def draw_collision_meta_data(self, surface):
         if self.d_lat is None or self.d_long is None:
@@ -181,15 +194,16 @@ class EgoVehicle(Vehicle):
             self.override_acc = -self.vel_inc
 
         if string == "l":
-            self.override_turn = np.sign(self.v_long) * self.turn_inc
+            self.override_turn = np.sign(self.v_long) * self.turn_rate_inc
 
         if string == "r":
-            self.override_turn = -np.sign(self.v_long) * self.turn_inc
+            self.override_turn = -np.sign(self.v_long) * self.turn_rate_inc
 
         if string == "m":
             self.self_driving = not self.self_driving
 
     def update(self, dt):
+
         super().update(dt)
 
         if self.perceiving:
@@ -248,7 +262,7 @@ class EgoVehicle(Vehicle):
         if self.override_turn is not None:
             self.overall_desired_turn_circle_change = self.override_turn
             self.override_turn = None
-        if abs(self.overall_desired_turn_circle_change) > 2 * self.turn_inc:
+        if abs(self.overall_desired_turn_circle_change) > 2 * self.turn_rate_inc:
             self.computed_v_long *= 0.5
             self.calculate_desired_acc()
 

model/Agent/RLEgoVehicle.py
@@ -17,7 +17,7 @@ class RLEgoVehicle(Vehicle):
         image_path = config.INPUT_IMAGE_FP + '/blue_car_top_down.png'
         super().__init__(image_path=image_path, world=world)
         self.world = world
-        self.set_goal([1210,330])
+        self.set_goal([0, 0])
 
     def set_goal(self, goal):
         self.goal=goal
@@ -34,9 +34,9 @@ class RLEgoVehicle(Vehicle):
         if string == "d":
             self.accelerate(-self.vel_inc)
         if string == "l":
-            self.turn_wheel(np.sign(self.v_long) * self.turn_inc)
+            self.turn_wheel(np.sign(self.v_long) * self.turn_rate_inc)
         if string == "r":
-            self.turn_wheel(-np.sign(self.v_long) * self.turn_inc)
+            self.turn_wheel(-np.sign(self.v_long) * self.turn_rate_inc)
 
     # endregion
 

model/Agent/Vehicle.py
@@ -26,11 +26,13 @@ class Vehicle:
         self.direction = np.deg2rad(starting_direction_degrees)
         self.vel_inc = 1  # ms^-2
         self.max_acc_rate = 5  # ms^-2
-        self.turn_inc = np.deg2rad(.3)  # rads per frame
-        self.turn_circle = 0  # rads per frame
-        self.max_turn_circle = np.deg2rad(3.5)  # rads per frame
+        self.turn_rate_inc = np.deg2rad(.3)  # rads per second
+        self.turn_rate = 0  # rads per frame
+        self.max_turn_rate = np.deg2rad(3.5)  # rads per second
         self.max_v = 30  # 30 is approximately 70mph
-        self.dt = np.inf # this is in milliseconds, so need to update all move functions
+        self.dt = 0  # this is in milliseconds, so need to update all move functions
+        self.mu = 1
+        self.g = 9.8
 
         # drawing/visualisation variables
         self.image_path = image_path
@@ -43,28 +45,28 @@ class Vehicle:
                           self.actual_vehicle_size[1] * self.pixel_to_metres_ratio]
 
         self.original_image = None
-        self.image= None
-        self.image_offset = [0,0]
+        self.image = None
+        self.image_offset = [0, 0]
         if config.DISPLAY_ON:
             self.original_image = pygame.image.load(self.image_path)
             self.original_image = pygame.transform.rotate(self.original_image, 180)
             self.image = pygame.transform.smoothscale(self.original_image, self.draw_size)
             self.rotate()
-
             self.image_offset = self.image.get_rect().center
+
         self.draw_offset = 0, 0
         self.draw_scale = 1
         self.world_x = -self.image_offset[0] + initial_position[0]
         self.world_y = -self.image_offset[1] + initial_position[1]
+
         self.draw_x = self.world_x
         self.draw_y = self.world_y
 
     def get_velocity(self):
-        return [self.v_long*np.cos(self.direction), self.v_long*np.sin(self.direction)]
+        return [self.v_long * np.cos(self.direction), self.v_long * np.sin(self.direction)]
 
     def get_position(self):
         return [self.world_x, self.world_y]
-
 
     # region utils
     def move(self):
@@ -78,35 +80,36 @@ class Vehicle:
         self.world_y -= y_addition
 
     def update(self, dt):
+
         self.dt = dt
-        self.move()
-        self.rotate(self.turn_circle)
+
         self.friction()
+        self.move()
+        self.rotate(self.turn_rate)
 
     def friction(self):
         # this should be based on an actual physical air resistance model,
         # and friction should maximise when speed is max_v, and at this point, it should equal the vel_inc value
 
         # if dt is too low, then the simulation takes far too long for the car to gets going.
-        if abs(self.v_long) > self.vel_inc*self.dt/1000:
-            k = 1 - np.power((self.max_v-abs(self.v_long))/self.max_v, 0.99) # ratio of 0 (zero speed) -1 (max speed)
-            v_delta = self.vel_inc * k * self.dt/1000
-            self.v_long -= np.sign(self.v_long)*v_delta
-        elif abs(self.v_long) < 0.999*self.vel_inc*self.dt/1000:
-            self.v_long=0
-            return
+        if abs(self.v_long) > self.vel_inc * self.dt / 1000:
+            k = 1 - np.power((self.max_v - abs(self.v_long)) / self.max_v,
+                             0.99)  # ratio of 0 (zero speed) -1 (max speed)
+            v_delta = self.vel_inc * k * self.dt / 1000
+            self.v_long -= np.sign(self.v_long) * v_delta
+        elif abs(self.v_long) < 0.999 * self.vel_inc * self.dt / 1000:
+            self.v_long = 0
 
-        # TODO: this should be dependent on the friction, not magic numbers
-        turn_circle = self.turn_circle - np.sign(self.turn_circle) * self.turn_inc * 0.9
-        if np.sign(self.turn_circle) != np.sign(turn_circle):
+        # TODO: this should be dependent on the friction, not magic numbers.
+        #  currently it subtracts 90% of the turn rate increment until zero
+        turn_circle = self.turn_rate - np.sign(self.turn_rate) * self.turn_rate_inc * 0.9
+        if np.sign(self.turn_rate) != np.sign(turn_circle):
             turn_circle = 0
-        self.turn_circle = turn_circle
+        self.turn_rate = turn_circle
 
     def draw(self, surface):
         ##### draw agent on surface#########
-
         self._create_image_instance()
-
         previous_image_center = self.image.get_rect().center
         rotated_image = pygame.transform.rotate(self.image, np.rad2deg(self.direction))
         rotated_image_center = rotated_image.get_rect().center
@@ -115,7 +118,6 @@ class Vehicle:
 
         self.image = rotated_image
         self.image_offset = [self.image_offset[0] + offset[0], self.image_offset[1] + offset[1]]
-
 
         self.update_offset(self.draw_offset)
         pos = (self.draw_x - self.image_offset[0], self.draw_y - self.image_offset[1])
@@ -126,36 +128,44 @@ class Vehicle:
         y = coord[1] * self.draw_scale + self.draw_offset[1]
         return x, y
 
-
     def send_message(self, string):
         ...
 
     def accelerate(self, amount):
         # accommodate the fact that dt is in milliseconds
-        dt_seconds = self.dt/1000
+        dt_seconds = self.dt / 1000
         if abs(amount) > self.vel_inc:
             amount = np.sign(amount) * self.vel_inc
-        v_long = self.v_long + amount*dt_seconds
+        v_long = self.v_long + amount * dt_seconds
         if abs(v_long) > abs(self.max_v):
             v_long = np.sign(self.v_long) * self.max_v
         self.v_long = v_long
 
     def turn_wheel(self, direction):
-        direction = np.sign(direction) * min(abs(direction), self.turn_inc)
-        if self.turn_circle == 0:
-            self.turn_circle = direction
+        # there is the maximum amount you can turn in any one frame, self.turn_inc
+        direction = np.sign(direction) * min(abs(direction), self.turn_rate_inc)
+        if self.turn_rate == 0:
+            self.turn_rate = direction
             return
-        turn_circle = direction + self.turn_circle
-        turn_circle = np.sign(turn_circle) * min(self.max_turn_circle, abs(turn_circle))
-        self.turn_circle = turn_circle
+
+        # this checks for the maximum turning circle
+        turn_circle = direction + self.turn_rate
+        turn_circle = np.sign(turn_circle) * min(self.max_turn_rate, abs(turn_circle))
+        self.turn_rate = turn_circle
 
     def rotate(self, angle_increment_radians=0):
-        self.direction += angle_increment_radians
+        # there is a minimum and a maximum amount that you can turn.
+        # this depends on your speed: too fast, and you can't turn for risk of slipping
+        # too slow, and you can't turn because of kinematic constraints.
+        # a better version is to implement an Ackermann controller, but this will do in a pinch.
+        # also, this is not appropriate for articulated vehicles
+
+
+        self.direction += self.dt/1000 * angle_increment_radians
         if self.direction < 0:
             self.direction += 2 * np.pi
         if abs(self.direction) > 2 * np.pi:
             self.direction = self.direction % (2 * np.pi)
-
 
     def update_offset(self, offset):
         self.draw_offset = offset

model/Environment/World.py
@@ -11,26 +11,28 @@ from model.ModelUtils import Waypoint
 """
 This is the environment
 Units in metric
+angles in radians
 """
 
 
 class World:
     def __init__(self, road_network=None, optimal_route=None):
-        self.draw_offset = 0,0
+        self.draw_offset = 0, 0
         self.waypoints = []
+        self.t = 0
 
         if road_network is not None and optimal_route is not None:
             for i, node_id in enumerate(optimal_route):
                 x = road_network.nodes[node_id]['x']
                 y = road_network.nodes[node_id]['y']
-                self.waypoints.append(Waypoint(i, x,y))
+                self.waypoints.append(Waypoint(i, x, y))
         else:
-            waypoints = [[25,0],[50, 50], [50, 100],[25, 125], [0, 125],[-25, 100], [-25, 25], [0, 0]]
+            waypoints = [[25, 0], [50, 50], [50, 100], [25, 125], [0, 125], [-25, 100], [-25, 25], [0, 0]]
             for i, val in enumerate(waypoints):
-                #self,guid, x, y, v_x=0, v_y=0, theta=0
-                self.waypoints.append(Waypoint(i, val[0], val[1], theta=4*np.pi*(np.random.random()-0.5)))
+                # self,guid, x, y, v_x=0, v_y=0, theta=0
+                self.waypoints.append(Waypoint(i, val[0], val[1], theta=4 * np.pi * (np.random.random() - 0.5)))
 
-        self.pygame_agents = []
+        self.all_agents = []
         self.static_obstacles = []
         self.dynamic_obstacles = []
         self.obstacles = shapely.geometry.MultiPolygon()
@@ -39,15 +41,14 @@ class World:
         self.original_images = []
         self.building_width = 50
 
-
         if config.OBSTACLES_ON:
             self.create_obstacles()
             self.obstacles = shapely.geometry.MultiPolygon(self.static_obstacles)
 
         self.ego_vehicle = EgoVehicle(world=self)
-        self.ego_vehicle.world_x=self.waypoints[0].position[0]
-        self.ego_vehicle.world_y=self.waypoints[0].position[1]
-        self.pygame_agents.append(self.ego_vehicle)
+        self.ego_vehicle.world_x = self.waypoints[0].position[0]
+        self.ego_vehicle.world_y = self.waypoints[0].position[1]
+        self.all_agents.append(self.ego_vehicle)
 
     def create_obstacles(self):
         build_1_path = config.INPUT_IMAGE_FP + 'Building_topdown_1.png'
@@ -79,14 +80,13 @@ class World:
                 self.obstacle_locations.append([x, y])
 
                 v = NormalVehicle(starting_position=(x - rect[2] / 2, y * 2.5), world=self)
-                self.pygame_agents.append(v)
+                self.all_agents.append(v)
 
                 x += scaled_image.get_width() + 100
 
-
     def update_offset(self, offset):
-        self.draw_offset =offset
-        for agent in self.pygame_agents:
+        self.draw_offset = offset
+        for agent in self.all_agents:
             agent.update_offset(offset)
 
     def update_scale(self, updated_zoom, previous_zoom):
@@ -98,17 +98,18 @@ class World:
             self.obstacle_images[i] = pygame.transform.smoothscale(self.original_images[i],
                                                                    (self.building_width, self.building_width))
 
-        for agent in self.pygame_agents:
+        for agent in self.all_agents:
             agent.update_scale(updated_zoom)
 
     def draw(self, surface):
         for i, obs in enumerate(self.obstacle_images):
-            x=self.obstacle_locations[i][0] + self.draw_offset[0]
-            y=self.obstacle_locations[i][1] + self.draw_offset[1]
-            surface.blit(obs,(x,y))
-        for agent in self.pygame_agents:
+            x = self.obstacle_locations[i][0] + self.draw_offset[0]
+            y = self.obstacle_locations[i][1] + self.draw_offset[1]
+            surface.blit(obs, (x, y))
+        for agent in self.all_agents:
             agent.draw(surface)
 
     def update(self, dt):
-        for agent in self.pygame_agents:
+        self.t += dt
+        for agent in self.all_agents:
             agent.update(dt)